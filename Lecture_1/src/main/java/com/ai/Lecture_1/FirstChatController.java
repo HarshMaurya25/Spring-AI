082fc3504fe551f14eeb6eb63da3d79db3156c52
Harsh <mauryaharsh2505@gmail.com>
2026-07-15 19:30:16 +0530

    Initial ymal configuration

---
+++ Lecture_1/src/main/java/com/ai/Lecture_1/FirstChatController.java
@@ -0,0 +1,29 @@
+package com.ai.Lecture_1;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Value;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Getter
+public class FirstChatController {
+
+    private final ChatClient chatClient;
+
+    private FirstChatController(ChatClient.Builder builder){
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public ResponseEntity<String> getFirstChat(
+            @RequestParam(value = "q" , required = true) String request
+    ){
+        return ResponseEntity.ok("This is the reponse");
+    }
+
+}
