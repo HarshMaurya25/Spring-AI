e419527f0a47301b070794688ef1e89ac613e78a
Harsh <mauryaharsh2505@gmail.com>
2026-07-16 22:33:30 +0530

    Implementation of the prompt

--- Lecture_1/src/main/java/com/ai/Lecture_1/FirstChatController.java
+++ Lecture_1/src/main/java/com/ai/Lecture_1/FirstChatController.java
@@ -3,6 +3,12 @@ package com.ai.Lecture_1;
 
 import lombok.Getter;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.content.Content;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,6 +31,23 @@ public class FirstChatController {
 
         var response = chatClient.prompt(request).call().content();
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/ask")
+    public ResponseEntity<String> askQuestion(
+            @RequestParam(value = "ask" , required = true) String request
+    ){
+        Prompt prompt = Prompt
+                .builder()
+                .messages(
+                        new UserMessage(request),
+                        new SystemMessage("Act as the Smart and genius spokesperson of the media company")
+                )
+                .build();
+
+        var response = chatClient.prompt(prompt).call().content();
+
+        return ResponseEntity.ok().body(response);
     }
 
 }
